1871bba55f023637907f6a77504b8104bd52b4dd
Douglas Fragoso <douglas.iff@gmail.com>
2024-08-24 16:04:29 -0300

    docs: update repository

---
+++ rest-spring-test/src/main/java/com/restspringtest/RestSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.restspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringTestApplication.class, args);
+	}
+
+}
